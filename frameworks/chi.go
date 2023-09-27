a78ef301ba9683fe1ee54c2fa1bc754a9a8a4351
Lionel <nope@not.today>
2023-09-27 10:40:23 +0200

    added chi

---
+++ frameworks/chi.go
@@ -0,0 +1,36 @@
+package frameworks
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"net/http"
+	"time"
+)
+
+var (
+	portChi = "5443"
+)
+
+func healthCheckEndpointChi(w http.ResponseWriter, r *http.Request) {
+	startTime := time.Now()
+	responseBody := map[string]string{
+		"message": "just some JSON",
+	}
+	responseJson, err := json.Marshal(responseBody)
+	if err != nil {
+		http.Error(w, "failed to marshal JSON", http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseJson)
+
+	totalTime := time.Since(startTime)
+	fmt.Printf("the route took %s long for the standard lib", totalTime)
+}
+
+func StartChiServer() {
+	r := chi.NewRouter()
+	r.Get("/healthCheck", healthCheckEndpointChi)
+	http.ListenAndServe(":"+portChi, nil)
+	fmt.Printf("Standard servers Listens on Port %s with provided endpoint /healthCheck\n", portChi)
+}
