80a8eb2ede115f633cc7b41cdc15aadc4dc1ef6c
BKR-dev <giga@sigmachad.quest>
2023-10-12 21:21:52 +0200

    provoking the dark bash magic

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 #
-#
-# read all files beginning with main_${}
-# execute go build -tags ${} -o ${}-router accordingly
-# check if all hs been built: exit code!?
-#bash
-# and then just ./${}
-#
 # check if stuff is already built, no need to do twice
+buildBinaries() {
 for f in main_*
 do
 	ROUTER_NAME=$(echo "$f" | cut -d"_" -f2 | cut -d"." -f1)
@@ -15,12 +9,23 @@ do
 	go build -tags $ROUTER_NAME -o $ROUTER_NAME-router
 done
 printf "Done building all binaries.\n"
+}
 
-# execute the binaries (DIFICULTY IMPOSSIBLE)
+
+startRouters() {
 printf "Starting all binaries via GNU parallel\n"
+binary_array=()
 for f in main_*
 do
 	ROUTER_NAME=$(echo "$f" | cut -d"_" -f2 | cut -d"." -f1)
-	printf "Starting $ROUTER_NAME-router in its own process...\n"
-	parallel ::: ./$ROUTER_NAME-router
+	binary_array+=("$ROUTER_NAME-router")
 done
+
+for ROUTER in "${binary_array[@]}"; do
+	printf "Starting $ROUTER in its own process...\n"
+	parallel ::: ./$ROUTER 
+done
+}
+
+buildBinaries
+startRouters
