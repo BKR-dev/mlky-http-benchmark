7e0031006ea48b2b2eb74a20c167978c2c1d20e5
BKR-dev <giga@sigmachad.quest>
2023-10-12 11:50:19 +0200

    making out a bit nicer, also testing my local parallels and oh boy its wierd

--- build.sh
+++ build.sh
@@ -11,12 +11,16 @@
 for f in main_*
 do
 	ROUTER_NAME=$(echo "$f" | cut -d"_" -f2 | cut -d"." -f1)
+	printf "Building $ROUTER_NAME as $ROUTER_NAME-router\n"
 	go build -tags $ROUTER_NAME -o $ROUTER_NAME-router
 done
+printf "Done building all binaries.\n"
 
 # execute the binaries (DIFICULTY IMPOSSIBLE)
+printf "Starting all binaries via GNU parallel\n"
 for f in main_*
 do
 	ROUTER_NAME=$(echo "$f" | cut -d"_" -f2 | cut -d"." -f1)
+	printf "Starting $ROUTER_NAME-router in its own process...\n"
 	parallel ::: ./$ROUTER_NAME-router
 done
